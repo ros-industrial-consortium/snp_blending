b79b7080bd0cb24676abb71bc1394dcbaa293780
Michael Ripperger <michael.ripperger@swri.org>
2025-07-23 21:27:43 -0500

    Added arguments to run docker in simulation; added docker compose override file

--- docker/launch_snp_blending.sh
+++ docker/launch_snp_blending.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 source /opt/snp_blending/install/setup.bash
-ros2 launch snp_blending start.launch.xml sim_robot:=false sim_vision:=false
+ros2 launch snp_blending start.launch.xml sim_robot:=${SNP_SIM_ROBOT:-false} sim_vision:=${SNP_SIM_VISION:-false} bypass_execution:=${SNP_BYPASS_EXECUTION:-false}
